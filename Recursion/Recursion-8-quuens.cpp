92f6de63423f7e5ecae4603e24c96940f598847a
lexxsh <john8538@naver.com>
2023-10-20 07:36:41 +0900

    hello

--- Recursion/Recursion-8-quuens.cpp
+++ Recursion/Recursion-8-quuens.cpp
@@ -1,34 +1,48 @@
-#include <iostream>
-#define MAX 15
+#include<iostream>
+#include<cmath>
+
 using namespace std;
+const int N = 8;
+int queensInRow[N];
+int cnt = 0;
 
-int col[MAX];
-int N, total = 0;
+void printQueens() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (queensInRow[i] == j) {
+                cout << "Q ";
+            }
+            else {
+                cout << "x ";
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
+    cnt++;
+}
 
-bool check(int level)
-{
-    for(int i = 0; i < level; i++)
-        if(col[i] == col[level] || abs(col[level] - col[i]) == level - i)
+bool check(int k, int i) {
+    for (int j = 0; j < k; j++)
+        if ((queensInRow[j] == i) || (abs(queensInRow[j] - i) == abs(j - k)))
             return false;
     return true;
 }
-
-void nqueen(int x)
-{
-    if(x == N)
-        total++;
-    else
-    {
-        for(int i = 0; i < N; i++)
-        {
-            col[x] = i;
-            if(check(x))
-                nqueen(x+1);
+void solve(int k) {
+    if (k == N) {
+        printQueens();
+    }
+    else {
+        for (int i = 0; i < N; i++) {
+            if (check(k, i)) {
+                queensInRow[k] = i;
+                solve(k + 1);
+            }
         }
     }
 }
 int main() {
-    cin >> N;
-    nqueen(0);
-    cout << total;
+    solve(0);
+    cout << "ÃÑ °¡´ÉÇÑ ¼ö : " << cnt << endl;
+    return 0;
 }
